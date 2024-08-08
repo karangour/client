10813941f8691532cac3b8e5cd3b557fdf0da72f
kgour <karangour@gmail.com>
2024-08-08 15:57:35 +0530

    asdf

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: process.env.NODE_ENV === 'production' ? '/client/' : '/',
+  base: process.env.NODE_ENV === "production" ? "/" : "/",
   server: { host: "0.0.0.0", port: 3000 },
   build: {
     outDir: "dist", // Ensure the build output directory is 'dist'
